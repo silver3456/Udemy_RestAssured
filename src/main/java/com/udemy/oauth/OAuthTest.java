239f25da77287c238260922c8ac368f9ebbddee6
silver3456 <avseliverov@gmail.com>
2025-06-10 19:37:28 +0300

    add a few more scenarios

--- src/main/java/com/udemy/oauth/OAuthTest.java
+++ src/main/java/com/udemy/oauth/OAuthTest.java
@@ -1,8 +1,11 @@
 package com.udemy.oauth;
 
+import com.udemy.pojo.Api;
 import com.udemy.pojo.GetCourse;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
@@ -36,6 +39,30 @@ public class OAuthTest {
 
         String linkedIn = gc.getLinkedIn();
         System.out.println(linkedIn);
+
+        // Get course title
+        String courseTitle = gc.getCourses().getApi().get(1).getCourseTitle();
+        System.out.println(courseTitle);
+
+        // Get course price without using index get(1) since it might change in the future
+        List<Api> apiList = gc.getCourses().getApi();
+        String coursePrice = null;
+        for (Api api : apiList) {
+            if (api.getCourseTitle().equalsIgnoreCase("SoapUI Webservices testing")) {
+                coursePrice = api.getPrice();
+                break;
+            }
+        }
+        System.out.println(coursePrice);
+
+        // Get price using stream
+        String price = apiList.stream()
+                .filter(s ->
+                        s.getCourseTitle().equalsIgnoreCase("SoapUI Webservices testing"))
+                .findFirst()
+                .map(Api::getPrice)
+                .orElse(null);
+        System.out.println(price);
 
 
     }
